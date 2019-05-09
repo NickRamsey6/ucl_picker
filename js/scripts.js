89e76587857979e300c30c8fc16b0a2ff69c8337
Nick Ramsey <nramseysc@gmail.com>
2019-05-09 09:53:15 -0700

    Initial project setup, semi-functional

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("#preview").click(function(event) {
+    var team1 = $("select#team1").val();
+    var team2 = $("select#team2").val();
+    $("#opening").hide();
+    $("#matchup").show();
+    if(team1==="Inter Milan 2009-2010") {
+      $("#T1Inter10").show();
+    }
+    else if(team1 ==="Chelsea 2011-2012") {
+      $("#T1Chelsea12").show();
+    }
+    if(team2==="Inter Milan 2009-2010") {
+      $("#T2Inter10").show();
+    }
+    else if(team2==="Chelsea 2011-2012") {
+      $("#T2Chelsea12").show();
+    }
+  })
+  $("#play").click(function(event) {
+    $("#matchup").hide();
+    $("#result").show();
+  })
+})
