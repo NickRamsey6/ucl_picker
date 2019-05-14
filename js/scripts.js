e5512321eadcb7e2a25c57878a4a44d91eb8ad79
Nick Ramsey <nramseysc@gmail.com>
2019-05-13 19:34:49 -0700

    set up objects and add spans for preview page

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
+//Biz Logic
+function Team(scorer, captain, manager) {
+  this.scorer = scorer;
+  this.captain = captain;
+  this.manager = manager;
+}
+
+var Chelsea = new Team("Drogba", "Terry", "DiMatteo");
+var Inter = new Team("Milito", "Zanetti", "Mourinho");
+
+
+
+
+
+
+
+
+
+
+
+
+//UI Logic
 $(document).ready(function() {
   $("#preview").click(function(event) {
     var team1 = $("select#team1").val();
@@ -6,6 +28,8 @@ $(document).ready(function() {
     $("#matchup").show();
     if(team1==="Inter Milan 2009-2010") {
       $("#T1Inter10").show();
+      $("#scorer").empty().append(Inter.scorer);
+      $("#captain").empty().append(Inter.captain);
     }
     else if(team1==="Chelsea 2011-2012") {
       $("#T1Chelsea12").show();
