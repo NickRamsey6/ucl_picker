6912f3e8ba08fd707029366ee8c712da8231d2de
Nick Ramsey <nramseysc@gmail.com>
2019-07-17 22:15:55 -0700

    css fix

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 //Biz Logic
 class Team {
-  constructor (scorer, captain, manager, pic) {
+  constructor (teamName, scorer, captain, manager, pic) {
+    this.teamName = teamName;
     this.scorer = scorer;
     this.captain = captain;
     this.manager = manager;
@@ -14,8 +15,8 @@ class Game {
 }
 
 
-var Chelsea = new Team("Drogba", "Terry", "DiMatteo", 'images/drogba.jpg');
-var Inter = new Team("Milito", "Zanetti", "Mourinho");
+var Chelsea = new Team("Chelsea 2011-2012", "Drogba", "Terry", "DiMatteo", 'images/drogba.jpg');
+var Inter = new Team("Inter Milan 2009-2010", "Milito", "Zanetti", "Mourinho");
 var game = new Game;
 game.array.push(Inter);
 game.array.push(Chelsea);
@@ -29,11 +30,7 @@ $(document).ready(function() {
   $("#preview").click(function(event) {
     var team1 = $("select#team1").val();
     var team2 = $("select#team2").val();
-    // document.getElementById("team1Scorer").innerHTML='<img src ="'+game.array[team1].pic+'">';
-    // console.log(team2);
-    // var img = document.createElement("IMG");
-    // img.src = "images/"+game.array[team1].pic;
-    // document.getElementById("#team1Scorer").appendChild(img);
+
     $("#team1Scorer").append(game.array[team1].scorer);
     document.getElementById("team1ScorerPic").src=game.array[team1].pic;
     $("#team1Captain").append(game.array[team1].captain);
@@ -41,7 +38,6 @@ $(document).ready(function() {
     $("#team2Scorer").append(game.array[team2].scorer);
     $("#team2Captain").append(game.array[team2].captain);
     $("#team2Manager").append(game.array[team2].manager);
-    // console.log(game.array[team2].captain);
     $("#opening").hide();
     $("#matchup").show();
 
