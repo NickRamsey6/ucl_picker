debf2b6c3fb5468d1c3ef9fce561635da5c7660d
Nick Ramsey <nramseysc@gmail.com>
2019-08-14 23:05:18 -0700

    matchup picture functionality for both teams

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
 //Biz Logic
 class Team {
-  constructor (teamName, scorer, captain, manager, pic) {
+  constructor (teamName, scorer, captain, manager, scorerPic, captainPic, managerPic) {
     this.teamName = teamName;
     this.scorer = scorer;
     this.captain = captain;
     this.manager = manager;
-    this.pic = pic;
+    this.scorerPic = scorerPic;
+    this.captainPic = captainPic;
+    this.managerPic = managerPic
   }
 }
 class Game {
@@ -15,8 +17,8 @@ class Game {
 }
 
 
-var Chelsea = new Team("Chelsea 2011-2012", "Drogba", "Terry", "DiMatteo", 'images/drogba.jpg');
-var Inter = new Team("Inter Milan 2009-2010", "Milito", "Zanetti", "Mourinho");
+var Chelsea = new Team("Chelsea 2011-2012", "Drogba", "Terry", "DiMatteo", 'images/chels1112/drogba.jpg', 'images/chels1112/jt.jpg', 'images/chels1112/dimatteo.jpg');
+var Inter = new Team("Inter Milan 2009-2010", "Milito", "Zanetti", "Mourinho", 'images/inter0910/milito.jpeg', 'images/inter0910/zanetti.jpeg', 'images/inter0910/mourinho.jpeg');
 var game = new Game;
 game.array.push(Inter);
 game.array.push(Chelsea);
@@ -32,12 +34,17 @@ $(document).ready(function() {
     var team2 = $("select#team2").val();
 
     $("#team1Scorer").append(game.array[team1].scorer);
-    document.getElementById("team1ScorerPic").src=game.array[team1].pic;
+    document.getElementById("team1ScorerPic").src=game.array[team1].scorerPic;
     $("#team1Captain").append(game.array[team1].captain);
+    document.getElementById("team1CaptainPic").src=game.array[team1].captainPic;
     $("#team1Manager").append(game.array[team1].manager);
+    document.getElementById("team1ManagerPic").src=game.array[team1].managerPic;
     $("#team2Scorer").append(game.array[team2].scorer);
+    document.getElementById("team2ScorerPic").src=game.array[team2].scorerPic;
     $("#team2Captain").append(game.array[team2].captain);
+    document.getElementById("team2CaptainPic").src=game.array[team2].captainPic;
     $("#team2Manager").append(game.array[team2].manager);
+    document.getElementById("team2ManagerPic").src=game.array[team2].managerPic;
     $("#opening").hide();
     $("#matchup").show();
 
